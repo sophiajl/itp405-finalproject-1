54c3b5a6c8a50e8ad97ae4cb14fa624c944a9c82
Michelle Huang <mhuang403@gmail.com>
2017-04-22 13:11:09 -0700

    no progress

--- resources/views/wine/index.blade.php
+++ resources/views/wine/index.blade.php
@@ -29,6 +29,10 @@
 </head>
 
 <style>
+
+    body{
+        font-family: Helvetica;
+    }
     #name{
         float:left;
         font-size:60px;
@@ -49,7 +53,7 @@
     .navlink {
         float:left;
         display: block;
-        width: 150px;
+        width: 200px;
         height: 40px;
         margin-top: 25px;
         color:white;
@@ -115,45 +119,15 @@
 <div id="outercontainer">
 
 
-    <?php
-//    if ( is_null($_SESSION['logged_in']) ) {
-//    ?>
-    <div style="position:relative;padding-left:30px;padding-right:30px;">
-        <a href="home.php" id="name"> Vinosaurus </a>
-        <div style="margin:auto;">
-            <a href="about.php" class="navlink">About Us</a>
 
-            <a href="sign_up.php" class="navlink"
-               style="float:right;font-size:20px;margin-top:35px;margin-left:20px;width:80px;">Sign Up</a>
-            <span style="color:white; font-size:15px;float:right;margin-top:40px;">or</span>
-            <a href="login.php" class="navlink" style="float:right;font-size:20px;margin-top:35px;width:100px;">Log-in</a>
-            <br style="clear:both;">
-        </div>
-    </div>
-<!--    --><?php
-//    }
-//    else if($_SESSION['logged_in']){
-//    ?>
     <div style="position:relative;padding-left:30px;padding-right:30px;">
         <a href="home.php" id="name"> Vinosaurus </a>
         <div style="margin:auto;">
             <a href="about.php" class="navlink">About Us</a>
             <a href="wine_add.php" class="navlink">Add a Wine</a>
 
-            <a href="logout.php" class="navlink" style="float:right;font-size:20px;margin-top:35px;margin-left:30px;width:100px;">Log-out</a>
-            <div style="color:white;float:right;font-size:20px;margin-top:35px;margin-left:20px;width:200px;">
-                Welcome,
-<!--                --><?php //echo $_SESSION['username']; ?>
-            </div>
 
-
-            <br style="clear:both;">
-        </div>
-    </div>
-<!--    --><?php
-//    }
-//    ?>
-    <img src="winerack.jpg" style="width:100%;opacity:0.8;">
+    <img src="http://www.wineshoplouisville.com/images/wine.jpg" height="500" style="width:100%;opacity:0.8;">
 
     <div id="body">
         <div style="float:left;text-align:center;center;font-size:30px; color:white;font-weight:bold;">
